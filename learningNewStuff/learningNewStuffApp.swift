12e8302bb9f8488b5372d7c21c267a731c856fe5
Rodrigo Silva Ribeiro <Rodrigo.ato@hotmail.com>
2022-10-29 11:24:05 -0300

    Initial Commit

---
+++ learningNewStuff/learningNewStuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningNewStuffApp.swift
+//  learningNewStuff
+//
+//  Created by Rodrigo Silva Ribeiro on 29/10/22.
+//
+
+import SwiftUI
+
+@main
+struct learningNewStuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
